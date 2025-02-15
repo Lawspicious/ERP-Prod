98ab92b93e3f7f65be73fe50464433cc5f4f2d68
Swapnil12022002 <swapnilmkab@gmail.com>
2025-02-15 22:20:01 +0530

    test commit

--- src/lib/config/firebase.config.ts
+++ src/lib/config/firebase.config.ts
@@ -1,4 +1,4 @@
-// firebase.ts
+// firebase.ts config
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
